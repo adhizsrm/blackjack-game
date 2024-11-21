1e13fe1c91d9fdea1cdb1f51f8687940259d8902
adhizsrm <adhizsrm@gmail.com>
2024-11-21 22:28:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,29 +8,30 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-    let randomNumber = Math.floor(Math.random()*13) + 1
-    if(randomNumber > 10) {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
         return 10
-    }
-    else if(randomNumber === 1) {
+    } else if (randomNumer === 1) {
         return 11
-    }
-    else {
-        return randomNumber
+    } else {
+        return randomNumer
     }
 }
+
 function startGame() {
+    isAlive = true
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
-    cards = [firstCard,secondCard]
-    sum = cards[0] + cards[1]
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
+
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
-    } 
+    }
     
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -41,16 +42,16 @@ function renderGame() {
     } else {
         message = "You're out of the game!"
         isAlive = false
-    }
+    } 
     messageEl.textContent = message
 }
 
 
 function newCard() {
-    if(isAlive === true && hasBlackJack === false) {
+    if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
-        renderGame()
+        renderGame()        
     }
 }
