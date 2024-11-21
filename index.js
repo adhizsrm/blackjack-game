f18758dc9e8307c05770177c4c281c122a65ff74
adhizsrm <adhizsrm@gmail.com>
2024-11-21 20:20:09 +0530

    Update index.js
    Assigning values in the startGame() function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -22,6 +20,10 @@ function getRandomCard() {
     }
 }
 function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard,secondCard]
+    sum = cards[0] + cards[1]
     renderGame()
 }
 function renderGame() {
